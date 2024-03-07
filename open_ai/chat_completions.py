68d6ccd9a824032e5b9f7d91ed18b899eaf2a9f6
Ryan Harris <ryan@synthesis.co.za>
2024-03-07 17:36:47 +0200

    Using beta AI Assistant

--- open_ai/chat_completions.py
+++ open_ai/chat_completions.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+import time
 
 load_dotenv()
 
@@ -43,5 +44,60 @@ def generate_image():
     # https://oaidalleapiprodscus.blob.core.windows.net/private/org-Ybu6ieCHE9HjcoIWRfId51HX/user-pFkgkIFFtFMJ1wmeBRKYjheq/img-MrJCQiMn7JIGN4KKYNO8JXlF.png?st=2024-03-06T13%3A47%3A12Z&se=2024-03-06T15%3A47%3A12Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-03-06T13%3A47%3A20Z&ske=2024-03-07T13%3A47%3A20Z&sks=b&skv=2021-08-06&sig=V68ISNh9ExL876%2BwLZ6pJc5hULdTU3JmplvypUUTbIk%3D
 
 
+def use_assistant():
+    assistant = client.beta.assistants.create(
+        name="Math Tutor",
+        instructions="You are a personal math tutor. Write and run code to answer math questions.",
+        tools=[{"type": "code_interpreter"}],
+        model="gpt-4-turbo-preview"
+    )
+
+    thread = client.beta.threads.create()
+
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+    )
+
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+        instructions="Please address the user as Ryan Harris. The user has a premium account."
+    )
+
+    attempts = 10
+    current = 1
+    busy = True
+
+    while busy and attempts != current:
+        check_run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id
+        )
+
+        if check_run.status == 'completed':
+            messages = client.beta.threads.messages.list(
+                thread_id=thread.id
+            )
+
+            for data in messages.data:
+                if data.role == 'assistant':
+                    content = data.content[0]
+                    print(content.text.value)
+
+            busy = False
+
+        if check_run.status == 'failed' or check_run.status == 'expired':
+            print('error retrieving')
+            busy = False
+
+        if check_run.status == 'queued' or check_run.status == 'in_progress':
+            print('queueing ' + str(current))
+            time.sleep(2)
+
+        current += 1
+
+
 def driver():
-    generate_text()
+    use_assistant()
